92458849f2993d8c2c3d10621e1f22f87400025a
Mouna EL AMRI <mouna.el-amri@capgemini.com>
2018-03-11 10:47:12 +0100

    First xcode project initialization

---
+++ velib-mobile-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  velib-mobile-ios
+//
+//  Created by cluster SIG on 11/03/2018.
+//  Copyright © 2018 velib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
