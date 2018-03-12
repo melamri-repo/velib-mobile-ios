254996d53fafe7573daf45bcf98df7fc03db2f01
EL AMRI <elamri.mouna@gmail.com>
2018-03-12 14:14:21 +0100

    init googleapi

--- velib-mobile-ios/AppDelegate.swift
+++ velib-mobile-ios/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyAnHj4gIP8HBUEB2Bxqzb5t_iTIr07Y-0c")
+        GMSPlacesClient.provideAPIKey("AIzaSyAnHj4gIP8HBUEB2Bxqzb5t_iTIr07Y-0c")
         return true
     }
 
