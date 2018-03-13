dd038b6d00dba419ae51c695d831a424e3e815e6
EL AMRI <elamri.mouna@gmail.com>
2018-03-13 14:47:38 +0100

    add MapViewController and location

--- velib-mobile-ios/AppDelegate.swift
+++ velib-mobile-ios/AppDelegate.swift
@@ -18,8 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyAcEk4GTcA17LztHEG-bY1C6tURWQ2oXAs")
-        GMSPlacesClient.provideAPIKey("AIzaSyAcEk4GTcA17LztHEG-bY1C6tURWQ2oXAs")
+        GMSServices.provideAPIKey("AIzaSyCJoM7GcdTC-NEHuUoRczLRJ_AVVBvJeXc")
+        GMSPlacesClient.provideAPIKey("AIzaSyCJoM7GcdTC-NEHuUoRczLRJ_AVVBvJeXc")
         return true
     }
 
